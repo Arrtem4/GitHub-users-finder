ef353d7546ec7131e4917dbd741c06bc1f9e6a09
Artem <yakovenko.art1@gmail.com>
2024-03-12 22:11:57 +0500

    clone repo

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    login: "Arrtem4",
+    avatar: "https://avatars.githubusercontent.com/u/111863167?v=4",
+    name: "Artem",
+    company: "",
+    blog: "",
+    location: "Russia",
+    bio: "React developer",
+    twitter: "",
+    repos: 9,
+    followers: 0,
+    following: 0,
+    created: "2022-08-23T09:29:28Z",
+};
